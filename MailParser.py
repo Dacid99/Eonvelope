21bc2e00d81466835a4ccc8daa04f4cf8d00459a
SBV <18516197-Dacid99@users.noreply.gitlab.com>
2024-08-01 12:29:15 +0200

    fixed encoding issue

--- MailParser.py
+++ MailParser.py
@@ -1,4 +1,5 @@
 import email 
+import email.header
 
 class MailParser:
     __fromString = "From"
@@ -8,38 +9,58 @@ class MailParser:
     __dateString = "Date"
     __subjectString = "Subject"
 
+    def __init__(self, mailToParse):
+        self.mailToParse = mailToParse
 
-    def parseToText(mail):
-        mailText = email.message_from_bytes(mail)
-        return mailText
-    
-    def parseFrom(mail):
-        mailText = MailParser.parseToText(mail)
-        return mailText.get(MailParser.__fromString)
-    
-    def parseTo(mail):
-        mailText = MailParser.parseToText(mail)
-        return mailText.get(MailParser.__toString)
-    
-    def parseBcc(mail):
-        mailText = MailParser.parseToText(mail)
-        return mailText.get(MailParser.__bccString)
-    
-    def parseCc(mail):
-        mailText = MailParser.parseToText(mail)
-        return mailText.get(MailParser.__ccString)
+    def decodeHeader(self, header):
+        decodedFragments = email.header.decode_header(header)
+        decodedString = ""
+        for fragment, charset in decodedFragments:
+            if charset is None:
+                decodedString += fragment.decode('utf-8', errors='replace') if isinstance(fragment, bytes) else fragment
+            else:
+                decodedString += fragment.decode(charset, errors='replace')
+        return decodedString
 
-    def parseDate(mail):
-        mailText = MailParser.parseToText(mail)
-        return mailText.get(MailParser.__dateString)
+    def decodeText(self, text):
+        charset = text.get_content_charset()
+        if charset == None:
+            charset = 'utf-8'
+        decodedText = text.get_payload(decode=True).decode(charset, errors='replace')
+        return decodedText
     
-    def parseSubject(mail):
-        mailText = MailParser.parseToText(mail)
-        return mailText.get(MailParser.__subjectString)
+    def parseToText(self):
+        mailMessage = email.message_from_bytes(self.mailToParse)
+        return mailMessage
     
-    def parseBody(mail):
-        mailText = MailParser.parseToText(mail)
+    def parseFrom(self):
+        mailText = self.parseToText()
+        return self.decodeHeader(mailText.get(MailParser.__fromString))
+    
+    def parseTo(self):
+        mailText = self.parseToText()
+        return self.decodeHeader(mailText.get(MailParser.__toString))
+    
+    def parseBcc(self):
+        mailText = self.parseToText()
+        return self.decodeHeader(mailText.get(MailParser.__bccString))
+    
+    def parseCc(self):
+        mailText = self.parseToText()
+        return self.decodeHeader(mailText.get(MailParser.__ccString))
+
+    def parseDate(self):
+        mailText = self.parseToText()
+        return self.decodeHeader(mailText.get(MailParser.__dateString))
+    
+    def parseSubject(self):
+        mailText = self.parseToText()
+        return self.decodeHeader(mailText.get(MailParser.__subjectString))
+    
+    def parseBody(self):
+        mailText = self.parseToText()
         mailBodyText = ""
         for text in mailText.walk():
             if text.get_content_type() == 'text/plain':
-                mailBodyText += text.as_string()
+                mailBodyText += self.decodeText(text)
+        return mailBodyText

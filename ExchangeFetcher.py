278b4a3323f8a6a06ba5d7604d8a444ae29ac3cd
SBV <18516197-Dacid99@users.noreply.gitlab.com>
2024-08-01 23:12:24 +0200

    made exchange use autodiscover, still untested

--- ExchangeFetcher.py
+++ ExchangeFetcher.py
@@ -2,17 +2,15 @@ import exchangelib
 from ExchangeMailParser import ExchangeMailParser
 
 class ExchangeFetcher:
-    def __init__(self, username, password, server='outlook.office365.com', primary_smtp_address=None, fullname=None, access_type=None, autodiscover=False, config=None, locale=None, default_timezone=None):
+    def __init__(self, username, password, primary_smtp_address=None, server='outlook.office365.com', fullname=None, access_type=exchangelib.DELEGATE, autodiscover=True, locale=None, default_timezone=None):
         self.__credentials = exchangelib.Credentials(username, password)
         self.__config = exchangelib.Configuration(server=server, credentials=self.__credentials)
-        self.__mailhost = exchangelib.Account(primary_smtp_address,
-                        fullname,
-                        access_type,
-                        autodiscover,
-                        self.__config,
-                        locale,
-                        default_timezone)
+        
+        self.__mailhost = exchangelib.Account(
+            primary_smtp_address=primary_smtp_address,fullname=fullname,access_type=access_type,autodiscover=autodiscover,locale=locale,default_timezone=default_timezone
+            )
         
     def fetchAllAndPrint(self):
         for message in self.__mailhost.inbox.all().order_by('-datetime_received')[:10]:
-            print(ExchangeMailParser.parseFrom(message))
+            mailParser = ExchangeMailParser(message)
+            print(mailParser.parseFrom())
---
+++ ExchangeTest.py
@@ -0,0 +1,5 @@
+from ExchangeFetcher import ExchangeFetcher
+
+mail = ExchangeFetcher('bt705637@myubt.de', 'QWC1reg4a27#', 'bt705637@myubt.de')
+
+mail.fetchAllAndPrint()

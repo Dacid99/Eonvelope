59171a9ee23eb2ade81d5e5d453e3aed0c7c8d6a
SBV <18516197-Dacid99@users.noreply.gitlab.com>
2024-08-02 23:00:25 +0200

    added init script for mariadb

---
+++ mariadb_init.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE IF NOT EXISTS email_archive;
+
+USE email_archive;
+
+CREATE TABLE IF NOT EXISTS emails (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    sender VARCHAR(255), 
+    date_received DATETIME,
+    bodytext TEXT
+);
+
+CREATE TABLE IF NOT EXISTS correspondents (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    email_name VARCHAR(255),
+    email_address VARCHAR(255) UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS email_correspondents (
+    email_id int,
+    correspondent_id int,
+    mention ENUM('TO', 'CC', 'BCC'),
+    FOREIGN KEY (email_id) REFERENCES emails(id) ON DELETE CASCADE,
+    FOREIGN KEY (correspondent_id) REFERENCES correspondents(id) ON DELETE CASCADE,
+    PRIMARY KEY (email_id, correspondent_id, mention)
+);

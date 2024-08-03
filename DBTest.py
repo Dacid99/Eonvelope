4e0b19297d5c47d622b85fb9b424dd8fa6d464d8
SBV <18516197-Dacid99@users.noreply.gitlab.com>
2024-08-03 22:55:16 +0200

    adapted test for db

--- DBTest.py
+++ DBTest.py
@@ -5,12 +5,13 @@ from MailParser import MailParser
 from DBManager import DBManager
 
 
-imapMail = IMAP_SSL_Fetcher(username="archiv@aderbauer.org", password="nxF154j9879ZZsW", host="imap.ionos.de", port=993)
-popMail = POP3_SSL_Fetcher(username="archiv@aderbauer.org", password="nxF154j9879ZZsW", host="pop.ionos.de")
-
-parsedLatestMail = imapMail.fetchLatest()
-
 with DBManager("192.168.178.109", "root", "example", "email_archive", "utf8mb4", "utf8mb4_bin") as db:
-    dbfeeder = EMailDBFeeder(db)
 
-    dbfeeder.insert(parsedLatestMail)
+    with IMAP_SSL_Fetcher(username="archiv@aderbauer.org", password="nxF154j9879ZZsW", host="imap.ionos.de", port=993) as imapMail:
+    #with POP3_SSL_Fetcher(username="archiv@aderbauer.org", password="nxF154j9879ZZsW", host="pop.ionos.de") as popMail:
+
+        parsedLatestMail = imapMail.fetchLatest()
+
+        dbfeeder = EMailDBFeeder(db)
+        if parsedLatestMail is not None:
+            dbfeeder.insert(parsedLatestMail)

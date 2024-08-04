2b66355fd09fb488c7620f6e9bf30c71f71e0f89
SBV <18516197-Dacid99@users.noreply.gitlab.com>
2024-08-04 20:47:14 +0200

    testing and todo update

--- DBTest.py
+++ DBTest.py
@@ -9,11 +9,11 @@ logging.basicConfig(level=logging.DEBUG)
 
 with DBManager("192.168.178.109", "root", "example", "email_archive", "utf8mb4", "utf8mb4_bin") as db:
 
-    #with IMAP_SSL_Fetcher(username="archiv@aderbauer.org", password="nxF154j9879ZZsW", host="imap.ionos.de", port=993) as imapMail:
-    with POP3_SSL_Fetcher(username="archiv@aderbauer.org", password="nxF154j9879ZZsW", host="pop.ionos.de") as popMail:
+    with IMAP_SSL_Fetcher(username="archiv@aderbauer.org", password="nxF154j9879ZZsW", host="imap.ionos.de", port=993) as imapMail:
+    #with POP3_SSL_Fetcher(username="archiv@aderbauer.org", password="nxF154j9879ZZsW", host="pop.ionos.de") as popMail:
 
-        #parsedNewMails = imapMail.fetchBySearch(searchCriterion="OLD")
-        parsedNewMails = popMail.fetchAll()
+        parsedNewMails = imapMail.fetchBySearch(searchCriterion="RECENT")
+        #parsedNewMails = popMail.fetchAll("")
 
         dbfeeder = EMailDBFeeder(db)
       
--- TODO.md
+++ TODO.md
@@ -1,1 +1,8 @@
-test pop3
+test exchange
+move sender to correspondents
+attachments
+eml files
+fix bodytext in outgoing
+methods for imap infos like mailbox names
+account db manager
+daemon with cli args

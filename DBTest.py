535246df7e00ab9e978664daea28dee7fdaf2590
SBV <18516197-Dacid99@users.noreply.gitlab.com>
2024-08-04 16:14:47 +0200

    refined test for db

--- DBTest.py
+++ DBTest.py
@@ -3,15 +3,20 @@ from IMAP_SSL_Fetcher import IMAP_SSL_Fetcher
 from POP3_SSL_Fetcher import POP3_SSL_Fetcher
 from MailParser import MailParser
 from DBManager import DBManager
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
 
 with DBManager("192.168.178.109", "root", "example", "email_archive", "utf8mb4", "utf8mb4_bin") as db:
 
-    with IMAP_SSL_Fetcher(username="archiv@aderbauer.org", password="nxF154j9879ZZsW", host="imap.ionos.de", port=993) as imapMail:
-    #with POP3_SSL_Fetcher(username="archiv@aderbauer.org", password="nxF154j9879ZZsW", host="pop.ionos.de") as popMail:
+    #with IMAP_SSL_Fetcher(username="archiv@aderbauer.org", password="nxF154j9879ZZsW", host="imap.ionos.de", port=993) as imapMail:
+    with POP3_SSL_Fetcher(username="archiv@aderbauer.org", password="nxF154j9879ZZsW", host="pop.ionos.de") as popMail:
 
-        parsedLatestMail = imapMail.fetchLatest()
+        #parsedNewMails = imapMail.fetchBySearch(searchCriterion="OLD")
+        parsedNewMails = popMail.fetchAll()
 
         dbfeeder = EMailDBFeeder(db)
-        if parsedLatestMail is not None:
-            dbfeeder.insert(parsedLatestMail)
+      
+        for mail in parsedNewMails:
+            print(mail)
+            dbfeeder.insert(mail)

8f6286f90a95c8c3eae76c603bb6e8cbf0f564b7
SBV <18516197-Dacid99@users.noreply.gitlab.com>
2024-08-10 12:05:19 +0200

    setup for database creation

--- Emailkasten/settings.py
+++ Emailkasten/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "Emailkasten.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "email_archive",
+        "USER": "root",
+        "PASSWORD": "example",
+        "HOST": '192.168.178.109',
+        "PORT": '3306'
+        }
 }
 
 

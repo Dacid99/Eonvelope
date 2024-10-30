c542842e7f1775edf974e3237ba77e8ff65318da
david <18516197-Dacid99@users.noreply.gitlab.com>
2024-10-30 19:41:20 +0100

    documentation: constants classes, incomplete, may prove unnecessary in hindsight

--- Emailkasten/constants.py
+++ Emailkasten/constants.py
@@ -19,6 +19,22 @@
 import os
 
 class MailFetchingCriteria:
+    """Namespace class for all implemented mail fetching criteria constants.
+    Note that IMAP does not support time just dates. So we are always refering to full days.
+    
+    Attributes:
+        RECENT (str): Filter by "RECENT" flag.
+        UNSEEN (str): Filter by "UNSEEN" flag.
+        ALL (str): Filter by "ALL" flag.
+        NEW (str): Filter by "NEW" flag.
+        OLD (str): Filter by "OLD" flag.
+        FLAGGED (str): Filter by "FLAGGED" flag.
+        ANSWERED (str): Filter by "ANSWERED" flag.
+        DAILY (str): Filter using "SENTSINCE" for mails sent the previous day or later.
+        WEEKLY (str): Filter using "SENTSINCE" for mails sent the previous week (counting back from now) or later.
+        MONTHLY (str): Filter using "SENTSINCE" for mails sent the previous 4 weeks (counting back from now) or later.
+        ANNUALLY (str): Filter using "SENTSINCE" for mails sent the previous 52 weeks (counting back from now) or later.
+    """
     RECENT = "RECENT"
     UNSEEN = "UNSEEN"
     ALL = "ALL"
@@ -32,6 +48,7 @@ class MailFetchingCriteria:
     ANNUALLY = "ANNUALLY"
 
     def __iter__(self):
+        """Method to allow easier referencing of the members by listing"""
         return iter((attr, value) for attr, value in self.__class__.__dict__.items() if not attr.startswith("__"))
 
     def __getitem__(self, key):
@@ -39,6 +56,15 @@ class MailFetchingCriteria:
 
 
 class MailFetchingProtocols:
+    """Namespace class for all implemented mail protocols constants.
+    
+    Attributes:
+        IMAP (str): The IMAP4 protocol.
+        IMAP_SSL (str): The IMAP4 protocol over SSL.
+        POP3 (str): The POP3 protocol.
+        POP3_SSL (str): The POP3 protocol over SSL.
+        EXCHANGE (str): Microsofts Exchange protocol.
+    """
     IMAP = "IMAP"
     IMAP_SSL = "IMAP_SSL"
     POP3 = "POP3"
@@ -46,25 +72,58 @@ class MailFetchingProtocols:
     EXCHANGE = "EXCHANGE"
 
 class EMailArchiverDaemonConfiguration:
+    """Namespace class for all configurations constants for the :class:`Emailkasten.EMailArchiverDaemon` instances.
+    
+    Attributes:
+        CYCLE_PERIOD_DEFAULT (int): The default cycle period of the daemons, in seconds, initially set to 60.
+        RESTART_TIME (int): The default restart time for the daemons in case of a crash, in seconds, initially set to 10.
+    """
     CYCLE_PERIOD_DEFAULT = 60
     RESTART_TIME = 10
 
 class StorageConfiguration:
+    """Namespace class for all configurations constants for the :class:`Emailkasten.Models.StorageModel`.
+    
+    Attributes:
+        MAX_SUBDIRS_PER_DIR (int): The maximum numbers of subdirectories in one storage unit. Initially set to 1000. Must not exceed 64000 for ext4 filesystem! 
+        STORAGE_PATH (str): The path to the storage for the saved data. Intially set to /mnt/archive. Must match the path in the docker-compose.yml to ensure data safety.
+        PRERENDER_IMAGETYPE (str): The image format for the prerendered eml files. Initially set to jpg.
+    """
     MAX_SUBDIRS_PER_DIR = 1000
     STORAGE_PATH = "/mnt/archive"
     PRERENDER_IMAGETYPE = 'jpg'
 
 class LoggerConfiguration:
+    """Namespace class for all configurations constants for the application loggers.
+    
+    Attributes:
+        APP_LOGFILE_PATH (str): The path to the Emailkasten logfile. Initially set to /var/log/Emailkasten.log . 
+        DJANGO_LOGFILE_PATH (str):  The path to the django logfile. Initially set to /var/log/django.log . 
+        APP_LOG_LEVEL (str):  The loglevel to the Emailkasten logfile. Is being set from an environment variable of the same name. Defaults to INFO. 
+        DJANGO_LOG_LEVEL (str): The loglevel to the django logfile. Is being set from an environment variable of the same name. Defaults to INFO. 
+        ROOT_LOG_LEVEL (str): The loglevel to the root console logger. Is being set from an environment variable of the same name. Defaults to INFO. 
+        LOGFILE_MAXSIZE (int): The maximum file size of a logfile. Initially set to 10 MiB.
+        LOGFILE_BACKUP_NUMBER (int): The maximum number of backup logfiles to keep. Initially set to 3.
+        LOG_FORMAT (str): The format of the log messages for all loggers. Initially set to '{asctime} {levelname} - {name}.{funcName}: {message}'.
+    """
     APP_LOGFILE_PATH = "Emailkasten.log" # /var/log/
     DJANGO_LOGFILE_PATH = "django.log"  # /var/log/
     APP_LOG_LEVEL = os.environ.get('APP_LOG_LEVEL', 'INFO')
     DJANGO_LOG_LEVEL = os.environ.get('DJANGO_LOG_LEVEL', 'INFO')
     ROOT_LOG_LEVEL = os.environ.get('ROOT_LOG_LEVEL', 'INFO')
-    LOGFILE_MAXSIZE = 10 * 1024 * 1024 # 10 MB
+    LOGFILE_MAXSIZE = 10 * 1024 * 1024 # 10 MiB
     LOGFILE_BACKUP_NUMBER = 3 
     LOG_FORMAT = '{asctime} {levelname} - {name}.{funcName}: {message}'
 
 class ParsingConfiguration:
+    """Namespace class for all configurations constants for the parsing of mails.
+    
+    Attributes:
+        CHARSET_DEFAULT (str): The default charset used for parsing of text. Initially set to utf-8.
+        STRIP_TEXTS (bool): Whether or not to strip whitespace from textfields like bodytext and subject. Initially set to True.
+        THROW_OUT_SPAM (bool): Whether or not to ignore emails that have a spam flag. Initially set to True.
+        APPLICATION_TYPES (list): A list of application types to parse as attachments. Initially set to ['application/pdf', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document']
+    """
     CHARSET_DEFAULT = 'utf-8'
     STRIP_TEXTS = True
     THROW_OUT_SPAM = True

f9e29ce64a6d5fc62b7c1c2db334a00f31b80ba1
David <18516197-Dacid99@users.noreply.gitlab.com>
2025-01-10 11:44:40 +0100

    test: many creation test for storagemodel, untested

--- test/models/test_StorageModel.py
+++ test/models/test_StorageModel.py
@@ -58,7 +58,7 @@ def fixture_mock_filesystem() -> Generator[FakeFilesystem, None, None]:
         ('conflicting-storage', True),
     ]
 )
-def test_StorageModel_initial_creation(STORAGE_PATH, expectedCritical, mock_logger, mock_filesystem, mocker):
+def test_StorageModel_initial_single_creation(STORAGE_PATH, expectedCritical, mock_logger, mock_filesystem, mocker):
     """Tests the correct initial allocation of storage by :class:`Emailkasten.Models.StorageModel.StorageModel`."""
 
     mock_StorageConfiguration = mocker.patch('Emailkasten.Models.StorageModel.StorageConfiguration')
@@ -76,6 +76,30 @@ def test_StorageModel_initial_creation(STORAGE_PATH, expectedCritical, mock_logg
     storage = StorageModel.objects.get(current=True)
     assert storage.directory_number == 0
     assert storage.path == os.path.dirname(subdirectory)
+    assert storage.subdirectory_count == 1
+
+
+@pytest.mark.django_db
+def test_StorageModel_initial_many_creation(STORAGE_PATH, expectedCritical, mock_logger, mock_filesystem, mocker):
+    """Tests the correct initial allocation of storage by :class:`Emailkasten.Models.StorageModel.StorageModel`."""
+
+    mock_StorageConfiguration = mocker.patch('Emailkasten.Models.StorageModel.StorageConfiguration')
+    mock_StorageConfiguration.STORAGE_PATH = STORAGE_PATH
+    mock_StorageConfiguration.MAX_SUBDIRS_PER_DIR = 3
+
+    for i in range(0,2*3+1):
+        StorageModel.getSubdirectory(f"test_{i}")
+
+    mock_logger.critical.assert_not_called()
+    mock_logger.info.assert_called()
+
+    assert StorageModel.objects.filter(current=True).count() == 1
+    assert StorageModel.objects.count() == 3
+    assert all(
+            storage.subdirectory_count == len(os.listdir(storage.path)) for storage in StorageModel.objects.all()
+        )
+    storage = StorageModel.objects.get(current=True)
+    assert storage.directory_number == 2
     assert storage.subdirectory_count == 1
 
 

d6e555e8052915432cba13634922e836157cd65c
david <18516197-Dacid99@users.noreply.gitlab.com>
2025-03-26 17:04:42 +0100

    improvement: moved allauth urls to web

--- Emailkasten/urls.py
+++ Emailkasten/urls.py
@@ -40,6 +40,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("health/", include("health_check.urls")),
-    path("users/", include("allauth.urls")),
     path("api/", include("api.urls")),
 ]

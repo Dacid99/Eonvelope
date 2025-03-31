f8b2b07d75d12c1e4e18deb89149b5c4854da543
david <18516197-Dacid99@users.noreply.gitlab.com>
2025-03-31 22:24:54 +0200

    test: added for webforms

---
+++ test/web/forms/correspondent_forms/test_BaseCorrespondentForm.py
@@ -0,0 +1,60 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+# Emailkasten - a open-source self-hostable email archiving server
+# Copyright (C) 2024  David & Philipp Aderbauer
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+"""Test module for the :class:`web.forms.correspondent_forms.BaseCorrespondentForm.BaseCorrespondentForm` form class."""
+
+import pytest
+from django.forms.models import model_to_dict
+
+from web.forms.correspondent_forms.BaseCorrespondentForm import BaseCorrespondentForm
+
+
+@pytest.mark.django_db
+def test_post(correspondentModel):
+    """Tests post direction of :class:`web.forms.correspondent_forms.BaseCorrespondentForm.BaseCorrespondentForm`."""
+    form = BaseCorrespondentForm(data=model_to_dict(correspondentModel))
+
+    assert form.is_valid()
+    form_data = form.cleaned_data
+    assert "email_name" in form_data
+    assert form_data["email_name"] == correspondentModel.email_name
+    assert "is_favorite" in form_data
+    assert form_data["is_favorite"] == correspondentModel.is_favorite
+    assert "email_address" not in form_data
+    assert "created" not in form_data
+    assert "updated" not in form_data
+    assert len(form_data) == 2
+
+
+@pytest.mark.django_db
+def test_get(correspondentModel):
+    """Tests get direction of :class:`web.forms.correspondent_forms.BaseCorrespondentForm.BaseCorrespondentForm`."""
+    form = BaseCorrespondentForm(instance=correspondentModel)
+    form_initial_data = form.initial
+    form_fields = form.fields
+
+    assert "email_name" in form_fields
+    assert "email_name" in form_initial_data
+    assert form_initial_data["email_name"] == correspondentModel.email_name
+    assert "is_favorite" in form_fields
+    assert "is_favorite" in form_initial_data
+    assert form_initial_data["is_favorite"] == correspondentModel.is_favorite
+    assert "email_address" not in form_fields
+    assert "created" not in form_fields
+    assert "updated" not in form_fields
+    assert len(form_fields) == 2

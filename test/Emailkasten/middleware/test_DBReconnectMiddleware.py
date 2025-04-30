80b1373c05cf9e0f9d2b028d7df920aeab23fec8
david <18516197-Dacid99@users.noreply.gitlab.com>
2025-04-30 19:19:35 +0200

    test (Emailkasten): DBReconnectMiddleware

---
+++ test/Emailkasten/middleware/test_DBReconnectMiddleware.py
@@ -0,0 +1,45 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+# Emailkasten - a open-source self-hostable email archiving server
+# Copyright (C) 2024  David & Philipp Aderbauer
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+"""Test module for the :class:`DBReconnectMiddleware`."""
+
+import pytest
+from django.db import OperationalError, connection
+
+from Emailkasten.constants import DatabaseConfiguration
+
+from ...web.views.conftest import owner_client
+
+
+@pytest.fixture
+def mock_time_sleep(mocker):
+    return mocker.patch("Emailkasten.middleware.DBReconnectMiddleware.time.sleep")
+
+
+@pytest.mark.django_db
+def test_reconnect(monkeypatch, owner_client, mock_time_sleep):
+    monkeypatch.setattr(
+        connection,
+        "cursor",
+        lambda: (_ for _ in ()).throw(OperationalError()),
+    )
+    with pytest.raises(OperationalError):
+        owner_client.get("/")
+
+    assert mock_time_sleep.call_count == DatabaseConfiguration.RECONNECT_RETRIES
+    mock_time_sleep.assert_called_with(DatabaseConfiguration.RECONNECT_DELAY)

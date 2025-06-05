c001766d4dba8f4eadc574596fdc2ea39e2f81b0
david <18516197-Dacid99@users.noreply.gitlab.com>
2025-06-05 11:29:22 +0200

    feat (docker): enabling and enforcing ssl

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 poetry run python manage.py migrate
 poetry run python manage.py createsuperuser --username=admin --email=  --noinput
-poetry run gunicorn Emailkasten.wsgi --bind 0.0.0.0:8000
+poetry run gunicorn Emailkasten.wsgi --bind 0.0.0.0:443 --keyfile=key.pem --certfile=cert.pem

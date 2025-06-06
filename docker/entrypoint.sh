0ae4ecb43f555fe1ce75f04ed404acc665a23030
david <18516197-Dacid99@users.noreply.gitlab.com>
2025-06-06 11:07:07 +0200

    docker: moved to alpine baseimage

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 poetry run python manage.py migrate
 poetry run python manage.py createsuperuser --username=admin --email=  --noinput
 poetry run gunicorn Emailkasten.wsgi --bind 0.0.0.0:443 --keyfile=key.pem --certfile=cert.pem

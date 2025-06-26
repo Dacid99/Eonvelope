dc33b6b6016ce0d49a54b104637d6b354e527780
david <18516197-Dacid99@users.noreply.gitlab.com>
2025-06-26 23:22:34 +0200

    fix (docker): removed hardcoded celery loglvl

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,9 +5,9 @@ echo "Starting rabbitmq ..."
 rabbitmq-server -detached
 echo "Successfully started rabbitmq."
 echo "Starting celery worker ..."
-poetry run celery -A Emailkasten worker --loglevel=info --detach
+poetry run celery -A Emailkasten worker --loglevel=${CELERY_LOG_LEVEL} --detach
 echo "Successfully started celery worker."
 echo "Starting celery beat ..."
-poetry run celery -A Emailkasten beat --loglevel=info -S django --detach
+poetry run celery -A Emailkasten beat --loglevel=${CELERY_LOG_LEVEL} -S django --detach
 echo "Successfully started celery beat."
 poetry run gunicorn Emailkasten.wsgi --bind 0.0.0.0:443 --keyfile=key.pem --certfile=cert.pem

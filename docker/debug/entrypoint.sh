b4ff22792f6ba86b3726f18cada3990f01ed3f5e
david <18516197-Dacid99@users.noreply.gitlab.com>
2025-08-21 00:06:01 +0200

    fix (docker): clelery loglevel didnt have fallback value

--- docker/debug/entrypoint.sh
+++ docker/debug/entrypoint.sh
@@ -5,9 +5,9 @@ echo "Starting rabbitmq ..."
 rabbitmq-server -detached
 echo "Successfully started rabbitmq."
 echo "Starting celery worker ..."
-poetry run celery -A src.Emailkasten worker --loglevel=${CELERY_LOG_LEVEL} --detach
+poetry run celery -A src.Emailkasten worker --loglevel=${CELERY_LOG_LEVEL:-INFO} --detach
 echo "Successfully started celery worker."
 echo "Starting celery beat ..."
-poetry run celery -A src.Emailkasten beat --loglevel=${CELERY_LOG_LEVEL} -S django --detach
+poetry run celery -A src.Emailkasten beat --loglevel=${CELERY_LOG_LEVEL:-INFO} -S django --detach
 echo "Successfully started celery beat."
 poetry run python3 manage.py runserver 0.0.0.0:8000 --insecure
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,9 +5,9 @@ echo "Starting rabbitmq ..."
 rabbitmq-server -detached
 echo "Successfully started rabbitmq."
 echo "Starting celery worker ..."
-poetry run celery -A src.Emailkasten worker --loglevel=${CELERY_LOG_LEVEL} --detach
+poetry run celery -A src.Emailkasten worker --loglevel=${CELERY_LOG_LEVEL:-INFO} --detach
 echo "Successfully started celery worker."
 echo "Starting celery beat ..."
-poetry run celery -A src.Emailkasten beat --loglevel=${CELERY_LOG_LEVEL} -S django --detach
+poetry run celery -A src.Emailkasten beat --loglevel=${CELERY_LOG_LEVEL:-INFO} -S django --detach
 echo "Successfully started celery beat."
 poetry run gunicorn src.Emailkasten.wsgi --bind 0.0.0.0:443 --keyfile=key.pem --certfile=cert.pem --workers=${GUNICORN_WORKER_NUMBER:-1} --access-logfile "/var/log/gunicorn_access.log"

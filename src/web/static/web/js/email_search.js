dc691c81c90a53e2127ce0b8864d0103ef842ab8
david <18516197-Dacid99@users.noreply.gitlab.com>
2026-01-24 20:24:48 +0100

    fix (web): email search bar js helper had bad queryselector

--- src/web/static/web/js/email_search.js
+++ src/web/static/web/js/email_search.js
@@ -2,7 +2,7 @@
   document.getElementById("emailSearch").addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const query = e.currentTarget.querySelector(".input").value.trim();
+    const query = e.currentTarget.querySelector("input").value.trim();
     if (query) {
       const encodedQuery = encodeURIComponent(query);
       window.location.href = `/emails/?search=${encodedQuery}`;

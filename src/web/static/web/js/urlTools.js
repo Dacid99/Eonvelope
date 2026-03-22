Dacid99/Eonvelope

commit 2eb19b774ad73878124f01bc63d455b05933be7c

style (api): remove trailing slashes from data endpoints

src/web/static/web/js/urlTools.js
@@ -1,7 +1,7 @@
 function buildApiDetailUrl(baseUrl, id) {
-    return new URL(id, baseUrl).href + "/";
+    return new URL(id, baseUrl).href;
 }
 
 function buildApiDetailActionUrl(baseUrl, id, action) {
-    return new URL(action, buildApiDetailUrl(id, baseUrl)).href + "/";
+    return new URL(action, buildApiDetailUrl(id, baseUrl)).href;
 }

src/api/v1/urls.py
@@ -41,7 +41,9 @@ from api.v1.views import (
 
 app_name = "v1"
 
-router = DefaultRouter()
+router = DefaultRouter(
+    trailing_slash=False
+)  # no slash for data endpoints for consistency with allauth headless api
 router.register("accounts", AccountViewSet, basename=AccountViewSet.BASENAME)
 router.register("mailboxes", MailboxViewSet, basename=MailboxViewSet.BASENAME)
 router.register("daemons", DaemonViewSet, basename=DaemonViewSet.BASENAME)
@@ -60,7 +62,7 @@ router.register("emails", EmailViewSet, basename=EmailViewSet.BASENAME)
 urlpatterns = [
     path("", include(router.urls)),
     path(
-        "stats/",
+        "stats",
         DatabaseStatsView.as_view(),
         name=DatabaseStatsView.NAME,
     ),

src/web/templates/web/attachment/partials/_multi_download-button.html
@@ -2,7 +2,7 @@
 
 <button class="btn btn-outline-primary"
         aria-label="{% translate 'Download' %}"
-        data-url="/api/v1/attachments/download/"
+        data-url="/api/v1/attachments/download"
         onclick="multiDownload(this)">
     <i class="fa fa-download" aria-hidden="true"></i>
 </button>

src/web/templates/web/correspondent/partials/_multi_download-button.html
@@ -2,7 +2,7 @@
 
 <button class="btn btn-outline-primary"
         aria-label="{% translate 'Download' %}"
-        data-url="/api/v1/correspondents/download/"
+        data-url="/api/v1/correspondents/download"
         onclick="multiDownload(this)">
     <i class="fa fa-download" aria-hidden="true"></i>
 </button>

src/web/templates/web/email/partials/_multi_download_dropdown.html
@@ -13,7 +13,7 @@
         {% for format, format_name in page_obj.0.mailbox.available_download_formats %}
             <li>
                 <button class="dropdown-item"
-                        data-url="/api/v1/emails/download/?file_format={{ format }}"
+                        data-url="/api/v1/emails/download?file_format={{ format }}"
                         onclick="multiDownload(this)">{{ format_name }}</button>
             </li>
         {% endfor %}

commit 587aeaa858a8cd2b6cad9b4588c4030b95402305

fix (web): urlTools stripped last part of api base, breaking multiactions bug introduced in 2eb19b77 fixes github issue #13 & gitlab issue #4

src/web/static/web/js/urlTools.js
@@ -1,7 +1,7 @@
-function buildApiDetailUrl(baseUrl, id) {
-	return new URL(id, baseUrl).href;
+function buildApiDetailUrl(apiBaseUrl, id) {
+	return new URL(id, `${apiBaseUrl}/`).href;
 }
 
-function buildApiDetailActionUrl(baseUrl, id, action) {
-	return new URL(action, buildApiDetailUrl(id, baseUrl)).href;
+function buildApiDetailActionUrl(apiBaseUrl, id, action) {
+	return new URL(action, buildApiDetailUrl(id, apiBaseUrl)).href;
 }